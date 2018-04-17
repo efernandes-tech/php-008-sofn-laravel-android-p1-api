6b1bfa57501727683c2b03689e1158bd1be28cc2
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-04-17 13:43:35 -0300

    alterando namespace da aplicacao

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace SON\Http\Controllers\Auth;
 
-use App\User;
-use App\Http\Controllers\Controller;
+use SON\User;
+use SON\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
