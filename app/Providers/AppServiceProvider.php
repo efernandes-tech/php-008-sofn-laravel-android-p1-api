fbdb25401584302d0a87bd52e0603ef780fcc2ec
edersonlrf <edersonluis.rf@gmail.com>
2018-04-20 12:11:39 -0300

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,9 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // if (!\App::environment('local')) {
-        //       \URL::forceSchema('https');
-        //  }
+        //
     }
 
     /**
